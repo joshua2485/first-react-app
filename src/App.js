98638069630d9157fbd933ec412854c4653d11e9
Joshua Selfe <josh@modjob.io>
2019-09-16 16:07:25 -0400

    attempt 5 at CICD

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Basic CICD Complete attempt 4
+          Basic CICD Complete attempt 5
         </p>
         <a
           className="App-link"
