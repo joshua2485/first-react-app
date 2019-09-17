aabeae83484e4baefcd3551a1c7f5c8569f03521
Joshua Selfe <josh@modjob.io>
2019-09-16 21:24:27 -0400

    attempt 6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Basic CICD Complete attempt 5
+          Basic CICD Complete attempt 6
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, it is fun!
+          Learn React
         </a>
       </header>
     </div>
